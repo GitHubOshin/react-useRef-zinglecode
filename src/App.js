30d6d5bf6ff46bded4857d9b187f20361c66462a
OSHIN <izkp44@gmail.com>
2023-02-22 05:27:44 +0700

    #12 useRef

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  const fileRef = useRef(null)
+
+  // function onFileChange(e) {
+  //   fileRef.current = e.target.files[0]
+  // }
+
+  function onUploadeClick(params) {
+    const myFile = fileRef.current.files[0]
+    console.log(myFile)
+  }
+
+  console.log('hehe')
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <h2>Hi</h2>
+        <p>
+          {/* <input type="file" onChange={onFileChange} /> */}
+          <input type="file" ref={fileRef} />
+        </p>
+        <p>
+          <button onClick={onUploadeClick}>Upload file</button>
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default App
